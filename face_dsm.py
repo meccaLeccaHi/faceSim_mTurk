a2e010df2d756495307c8a80895b20b67230708b
lab <ajones173@gmail.com>
2017-04-07 14:39:19 -0500

    finished initial figure design

--- face_dsm.py
+++ face_dsm.py
@@ -58,7 +58,7 @@ plt.xlabel('face-pair')
 plt.ylabel('HIT')
 plt.title('normalized responses')
 plt.colorbar()
-plt.show()
+#plt.show()
 
 fig.savefig(main_dir+'results/norm_resps.png', facecolor=fig.get_facecolor(), edgecolor='none')
 
@@ -81,8 +81,9 @@ for i in range(FPAIR1[-1]+1):
 
 fig, ax = plt.subplots(1, figsize=(10,10), facecolor='black')
 
-axS = ax.matshow(resp_mat,cmap='hot',extent=[1,len(FNAMES)+1,1,len(FNAMES)+1],aspect=1)
-ax.axison = False
+plt.subplot(111)
+plt.imshow(resp_mat,interpolation='none',cmap='hot') # ,aspect=1
+#plt.colorbar(orientation='horizontal')
 
 xl, yl, xh, yh=np.array(ax.get_position()).ravel()
 w = xh-xl
@@ -94,7 +95,8 @@ crop_size = 20
 for i,x in enumerate(FACE_URL):
     
     xp = xl+(size*i)
-    
+    yp = yl+(size*i)
+
     # Load image using URL
     file = cStringIO.StringIO(urllib.urlopen(x).read())
     face_img = Image.open(file)
@@ -102,20 +104,31 @@ for i,x in enumerate(FACE_URL):
     # Crop image edges
     face_img = face_img.crop((crop_size,crop_size,face_img.size[0]-crop_size,face_img.size[1]-crop_size))
 
+    # Plot on x-axis
     ax1=fig.add_axes([xp, yh-0.01, size, size])
     ax1.axison = False
     imgplot = ax1.imshow(face_img)
 
-    yp = yl+(size*i)
-
+    # Plot on y-axis
     ax2=fig.add_axes([xl-size-0.01, yp+0.02, size, size])
     ax2.axison = False
     imgplot = ax2.imshow(face_img)
 
+# Create colorbar content
+foo = np.array([range(100) for x in range(3)])
+
+# Plot colorbar manually
+ax1 = fig.add_axes([xl, 0.05, w, h/13.5])
+im = ax1.imshow(foo,interpolation='nearest',cmap='hot')
+ax1.set_xlabel('mean similarity',color='white')
+ax1.set_xticks((0,100))
+ax1.set_yticks([])
+temp_lims = (round(min(mean_resp)*1000)/1000,round(max(mean_resp)*1000)/1000)
+ax1.set_xticklabels(temp_lims,color='white')
 
 plt.show()
 
-fig.savefig(main_dir+'results/face_dsm.png', facecolor=fig.get_facecolor(), edgecolor='none')
+fig.savefig(main_dir+'results/face_dsm.png',dpi=200,facecolor=fig.get_facecolor(), edgecolor='none')
 
 pass
 
